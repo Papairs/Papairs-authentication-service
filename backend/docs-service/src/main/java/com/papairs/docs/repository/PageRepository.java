527337c4cafb41c4f14b13d73ded4599f760ba22
Patrick Pini <marc6571us@gmail.com>
2025-11-02 19:37:23 +0100

    Added folder-based queries to PageRepository
    Introduced methods to find, check existence, count and delete pages by folder ID in PageRepository

--- backend/docs-service/src/main/java/com/papairs/docs/repository/PageRepository.java
+++ backend/docs-service/src/main/java/com/papairs/docs/repository/PageRepository.java
@@ -2,6 +2,8 @@ package com.papairs.docs.repository;
 
 import com.papairs.docs.model.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,4 +17,33 @@ public interface PageRepository extends JpaRepository<Page, String> {
      * @return List of pages owned by the user
      */
     List<Page> findByOwnerId(String ownerId);
+
+    /**
+     * Find pages by folder ID
+     * @param folderId folder ID
+     * @return List of pages in the folder
+     */
+    List<Page> findByFolderId(String folderId);
+
+    /**
+     * Check if any pages exist in the folder
+     * @param folderId folder ID
+     * @return true if pages exist, else false
+     */
+    boolean existsByFolderId(String folderId);
+
+    /**
+     * Count pages in the folder
+     * @param folderId folder ID
+     * @return number of pages in the folder
+     */
+    long countByFolderId(String folderId);
+
+    /**
+     * Delete pages by folder ID
+     * @param folderId folder ID
+     */
+    @Modifying
+    @Query("DELETE FROM Page p WHERE p.folderId = :folderId")
+    void deleteByFolderId(String folderId);
 }
