d4e1fc462046dc23b85594620054a8da43934bf3
Patrick Pini <marc6571us@gmail.com>
2025-11-03 11:14:45 +0100

    Added Javadoc comments to FolderRepository methods
    Added detailed Javadoc comments to all methods in FolderRepository to improve code documentation and clarify method purposes and parameters

--- backend/docs-service/src/main/java/com/papairs/docs/repository/FolderRepository.java
+++ backend/docs-service/src/main/java/com/papairs/docs/repository/FolderRepository.java
@@ -8,9 +8,38 @@ import java.util.List;
 
 @Repository
 public interface FolderRepository extends JpaRepository<Folder, String> {
+    /**
+     * Find folders by owner ID
+     * @param ownerId owner's user ID
+     * @return List of Folder entities
+     */
     List<Folder> findByOwnerId(String ownerId);
+
+    /**
+     * Find root folders by owner ID
+     * @param ownerId owner's user ID
+     * @return List of root Folder entities
+     */
     List<Folder> findByOwnerIdAndParentFolderIdIsNull(String ownerId);
+
+    /**
+     * Find folders by parent folder ID
+     * @param parentFolderId parent folder ID
+     * @return List of Folder entities
+     */
     List<Folder> findByParentFolderId(String parentFolderId);
+
+    /**
+     * Check if any folders exist in the parent folder
+     * @param parentFolderId parent folder ID
+     * @return true if folders exist, else false
+     */
     boolean existsByParentFolderId(String parentFolderId);
+
+    /**
+     * Count folders in the parent folder
+     * @param parentFolderId parent folder ID
+     * @return number of folders in the parent folder
+     */
     long countByParentFolderId(String parentFolderId);
 }
