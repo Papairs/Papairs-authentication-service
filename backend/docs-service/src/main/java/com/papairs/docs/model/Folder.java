df81bb478481c5f86a1271da83ec2b9ca3f017cb
Patrick Pini <marc6571us@gmail.com>
2025-11-12 00:08:40 +0100

    Made Folder model create server-sided for testing

--- backend/docs-service/src/main/java/com/papairs/docs/model/Folder.java
+++ backend/docs-service/src/main/java/com/papairs/docs/model/Folder.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
 
@@ -32,6 +33,11 @@ public class Folder {
     @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
+
+    @PrePersist
+    void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
 
     public String getFolderId() {
         return folderId;
