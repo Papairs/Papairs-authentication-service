dd73ee4e747acd98e547264714ba807d4eb00f34
Patrick Pini <marc6571us@gmail.com>
2025-11-25 10:26:16 +0100

    Reintroduced CorsConfig because application.yml was ignored

---
+++ backend/orchestration-service/src/main/java/com/papairs/orchestration/config/CorsConfig.java
@@ -0,0 +1,34 @@
+package com.papairs.orchestration.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${CORS_ALLOWED_ORIGIN:http://localhost:3000}")
+    private String allowedOrigin;
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+
+        corsConfig.setAllowedOrigins(List.of(allowedOrigin));
+
+        corsConfig.setMaxAge(3600L);
+        corsConfig.addAllowedMethod("*");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
+}
